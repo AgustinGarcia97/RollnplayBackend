e3c85ba9b9e8f3bd55b93f207c22c696ba30aa78
Agustin <aggarcia997@gmail.com>
2024-05-17 20:05:21 -0300

    se implemento PlayerDTO

---
+++ src/main/java/com/example/serverapi/dto/PlayerDTO.java
@@ -0,0 +1,15 @@
+package com.example.serverapi.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+@Data
+@NoArgsConstructor
+public class PlayerDTO {
+    private Long playerId;
+    private String numberOfPlayers;
+    private Long productId;
+
+
+}
