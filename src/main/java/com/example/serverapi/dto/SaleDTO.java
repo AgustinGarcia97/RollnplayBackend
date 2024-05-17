69c74583fffe7c8cb4ab39b68ffb8d76f7090373
Agustin <aggarcia997@gmail.com>
2024-05-17 19:30:32 -0300

    se implemento SaleDTO

--- src/main/java/com/example/serverapi/dto/SaleDTO.java
+++ src/main/java/com/example/serverapi/dto/SaleDTO.java
@@ -10,6 +10,10 @@ import java.util.UUID;
 @Data
 @NoArgsConstructor
 public class SaleDTO {
-
+    private Long saleId;
+    private LocalDateTime saleDate;
+    private UUID userId;
+    private List<Long> productIds;
+    private List<Long> listingsId;
 
 }
