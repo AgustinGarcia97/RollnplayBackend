d4adefcccdaac3b07f97e1fbd7c0d4d0703d72f2
Agustin <aggarcia997@gmail.com>
2024-05-23 19:10:05 -0300

    Se agrego e implemento el metodo getProductByProductName(String productName) que retorna un Product por su nombre

--- src/main/java/com/example/serverapi/database/service/ListingService.java
+++ src/main/java/com/example/serverapi/database/service/ListingService.java
@@ -1,4 +1,23 @@
 package com.example.serverapi.database.service;
 
+
+import com.example.serverapi.database.repository.ListingRepository;
+import com.example.serverapi.model.Listing;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ListingService {
+    @Autowired
+    private ListingRepository listingRepository;
+
+
+    public void createListing(Listing listing) {
+        listingRepository.save(listing);
+    }
+
+
+
+
+
 }
