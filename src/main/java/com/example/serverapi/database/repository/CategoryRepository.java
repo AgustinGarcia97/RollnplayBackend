85670189249bfa9341ca5153f59c10cea4681cab
Agustin <aggarcia997@gmail.com>
2024-05-24 04:53:42 -0300

    Se asignaron los beans correspondientes  @Repository faltantes

--- src/main/java/com/example/serverapi/database/repository/CategoryRepository.java
+++ src/main/java/com/example/serverapi/database/repository/CategoryRepository.java
@@ -2,7 +2,9 @@ package com.example.serverapi.database.repository;
 
 import com.example.serverapi.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
    Category findFirstByCategoryName(String categoryName);
 }
--- src/main/java/com/example/serverapi/database/repository/PlayerRepository.java
+++ src/main/java/com/example/serverapi/database/repository/PlayerRepository.java
@@ -2,7 +2,9 @@ package com.example.serverapi.database.repository;
 
 import com.example.serverapi.model.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PlayerRepository extends JpaRepository<Player, Long> {
     Player findByNumberOfPlayers(String numberOfPlayers);
 }
