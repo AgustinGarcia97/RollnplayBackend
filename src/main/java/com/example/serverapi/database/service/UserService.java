c84c7af5d1fc5abac048fb057923073cb964fb57
Agustin <aggarcia997@gmail.com>
2024-05-24 20:50:58 -0300

    se agrego la logica para guardar o actualizar un usuario

--- src/main/java/com/example/serverapi/database/service/UserService.java
+++ src/main/java/com/example/serverapi/database/service/UserService.java
@@ -2,11 +2,13 @@ package com.example.serverapi.database.service;
 
 
 import com.example.serverapi.database.repository.UserRepository;
+import com.example.serverapi.model.Listing;
 import com.example.serverapi.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -18,7 +20,15 @@ public class UserService {
         return userRepository.findAll();
     }
 
-    public User getUserById(UUID id) {
-        return userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
+    public void createOrUpdateUser(User user) {
+        userRepository.save(user);
+    }
+
+    public Optional<User> getUserById(UUID id) {
+        return userRepository.findById(id);
+    }
+
+    public List<Listing> getListingById(UUID id){
+       return userRepository.findById(id).get().getListings();
     }
 }
