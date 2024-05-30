5fb7748b30993c2f75976fb076fb8d23128b03be
Mariano <mariano_ef@live.com>
2024-05-30 16:18:54 -0300

    fixes sale - security

--- src/main/java/com/example/serverapi/controller/SaleController.java
+++ src/main/java/com/example/serverapi/controller/SaleController.java
@@ -6,6 +6,7 @@ import com.example.serverapi.model.Sale;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,12 +15,36 @@ public class SaleController {
     private SaleService saleService;
 
     @PostMapping("/create-sale")
-    public ResponseEntity<?> createSale (@RequestParam Integer id) throws UserNotFoundException {
-        try {
-            saleService.createSale(id);
-            return new ResponseEntity<>(HttpStatus.OK);
-        } catch (UserNotFoundException e) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    public ResponseEntity<?> createSale (@RequestParam String id) throws UserNotFoundException {
+
+        // Verificamos si el usuario envió el param
+        if (id == null) {
+            return new ResponseEntity<>("ID no recibido", HttpStatus.BAD_REQUEST);
         }
+
+        System.out.println(id);
+        // Verificamos si el param es un integer
+        if (id.isEmpty()) {
+            return new ResponseEntity<>("ID no recibido", HttpStatus.BAD_REQUEST);
+        } else {
+            try {
+                Integer id_int = Integer.parseInt(id);
+                try {
+                    saleService.createSale(id_int);
+                    return new ResponseEntity<>(HttpStatus.OK);
+                } catch (UserNotFoundException e) {
+                    return new ResponseEntity<>("ID de usuario no encontrado", HttpStatus.NOT_FOUND);
+                }
+            } catch (NumberFormatException e) {
+                return new ResponseEntity<>("Formato de ID incorrecto", HttpStatus.BAD_REQUEST);
+            }
+        }
+    }
+
+    // Excepción para revisar si el param es recibido
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public String handleMissingParams(MissingServletRequestParameterException ex) {
+        String paramName = ex.getParameterName();
+        return "El parámetro '" + paramName + "' es requerido";
     }
 }
