8b56dffc2f2363b2651854030179defaa5c71263
Agustin <aggarcia997@gmail.com>
2024-06-07 04:13:53 -0300

    Se inicio la modificacion y la reasignacion de codigo a sus correspondientes clases y metodos.
    En este commit se inicio con Product (createOrUpdateProduct) que llama al conversor, el cual ahora es un conversor independiente por cada entidad y luego lo retorna para guardarlo en la DB

--- src/main/java/com/example/serverapi/database/service/ProductService.java
+++ src/main/java/com/example/serverapi/database/service/ProductService.java
@@ -3,10 +3,13 @@ package com.example.serverapi.database.service;
 import com.example.serverapi.database.repository.CategoryRepository;
 import com.example.serverapi.database.repository.PlayerRepository;
 import com.example.serverapi.database.repository.ProductRepository;
+import com.example.serverapi.dto.ProductDTO;
 import com.example.serverapi.model.Category;
 import com.example.serverapi.model.Player;
 import com.example.serverapi.model.Product;
+import com.example.serverapi.utils.Converter.ProductConverter;
 import jakarta.persistence.EntityManager;
+import org.hibernate.HibernateException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -30,36 +33,30 @@ public class ProductService {
     @Autowired
     private CategoryRepository categoryRepository;
 
+    @Autowired
+    ProductConverter productConverter;
+
     public Product findByName(String name) {
         return productRepository.findByProductName(name);
     }
+
+    public Product findById(Long id) {return productRepository.findById(id).get();}
 
 
     public Optional<Product> findById(long id) {
         return productRepository.findById(id);
     }
 
-    public Product createProduct(Product product) {
-        Product p = null;
-
-        //Busca la si el registro numberOfPlayers si existe
-        String numberOfPlayers = product.getPlayers().getNumberOfPlayers();
-        Player existingPlayer = playerRepository.findByNumberOfPlayers(numberOfPlayers);
-        //Si la cantDeJugadores ya existe en la tabla Players, no la crea, sino que agrega la que ya existe
-        if (existingPlayer != null) {
-            product.setPlayers(existingPlayer);
+    public Product createOrUpdateProduct(ProductDTO productDTO) {
+        Product product = productConverter.convertToEntity(productDTO);
+        try{
+             productRepository.save(product);
+        }
+        catch(HibernateException e){
+            System.out.println(e);
         }
 
-        //Busca la categoria si existe
-        String categoryName = product.getCategory().getCategoryName();
-        Category categoryExisting = categoryRepository.findFirstByCategoryName(categoryName);
-        //Si la categoria ya existe en la tabla Category, no la crea, sino que agrega la que ya existe
-        if(categoryExisting != null) {
-            product.setCategory(categoryExisting);
-        }
-
-        p = productRepository.save(product);
-        return p;
+        return product;
     }
 
 
---
+++ src/main/java/com/example/serverapi/utils/Converter/ProductConverter.java
@@ -0,0 +1,14 @@
+package com.example.serverapi.utils.Converter;
+
+import com.example.serverapi.dto.ProductDTO;
+import com.example.serverapi.model.Product;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProductConverter {
+
+    public Product convertToEntity(ProductDTO productDTO) {
+        return null;
+
+    }
+}
