ede8819284a7ca7ff52e2ba348179abcd4bad025
Agustin <aggarcia997@gmail.com>
2024-06-07 18:24:24 -0300

    Se implemento el metodo convertToEntity para la entidad User. Se debe implementar para listing, sale y purchase

--- src/main/java/com/example/serverapi/utils/Converter/UserConverter.java
+++ src/main/java/com/example/serverapi/utils/Converter/UserConverter.java
@@ -1,4 +1,32 @@
 package com.example.serverapi.utils.Converter;
 
+import com.example.serverapi.dto.UserDTO;
+import com.example.serverapi.model.Role;
+import com.example.serverapi.model.User;
+import org.springframework.stereotype.Component;
+
+
+@Component
 public class UserConverter {
+
+    public User convertToEntity(UserDTO userDTO){
+        User user = new User();
+        if(userDTO.getUserId() != null){
+            user.setUserId(userDTO.getUserId());
+        }
+        user.setUsername(userDTO.getUsername());
+        user.setPassword(userDTO.getPassword());
+        user.setAddress(userDTO.getAddress());
+        user.setEmail(userDTO.getMail());
+        user.setName(userDTO.getName());
+        user.setPhoneNumber(userDTO.getPhoneNumber());
+        user.setDocument(userDTO.getDocument());
+        user.setRole(Role.valueOf(userDTO.getRole()));
+
+        user.setListings();
+        user.setPurchases();
+        user.setSales();
+
+        return user;
+    }
 }
