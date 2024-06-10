d1d9e865f4ad40d21220a247e63b3b022f509bca
Agustin <aggarcia997@gmail.com>
2024-06-10 10:30:13 -0300

    Se crearon los endpoints del CRUD correspondiente, como tambien su service para la entidad Image

--- src/main/java/com/example/serverapi/controller/ImageController.java
+++ src/main/java/com/example/serverapi/controller/ImageController.java
@@ -5,39 +5,71 @@ import com.example.serverapi.database.service.ImageService;
 import com.example.serverapi.dto.ImageDTO;
 import com.example.serverapi.model.Image;
 import com.example.serverapi.utils.DTOConverter;
+import com.example.serverapi.utils.converter.DtoAssembler;
 import com.example.serverapi.validator.ListingValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 public class ImageController {
 
-    @Autowired
-    private DTOConverter dtoConverter;
+    private final ImageService imageService;
+
 
     @Autowired
-    private ImageService imageService;
+    public ImageController(ImageService imageService) {
+        this.imageService = imageService;
 
-    @DeleteMapping("/delete-photo")
-    public ResponseEntity<String> deletePhoto(@RequestParam List<ImageDTO> imageDTO) {
+    }
 
+    @GetMapping("/get-image")
+    public ResponseEntity<ImageDTO> getImage(@RequestParam Long id) {
         try{
-
-            for(ImageDTO imageDTO1: imageDTO){
-                Image image = dtoConverter.convertToImage(imageDTO1);
-                imageService.deleteImage(image);
-            }
-
-            return new ResponseEntity<>("Imagenes eliminadas correctamente", HttpStatus.OK);
+            ImageDTO imageDTO = imageService.getImageDTOById(id);
+            return new ResponseEntity<>(imageDTO, HttpStatus.OK);
         }
         catch(Exception e){
-            return new ResponseEntity<>("An unexpected error occurred: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
+
+    @PostMapping("/create-image")
+    public ResponseEntity<ImageDTO> createImage(@RequestBody ImageDTO imageDTO) {
+        try{
+            imageService.createOrUpdateImage(imageDTO);
+            return new ResponseEntity<>(imageDTO, HttpStatus.CREATED);
+        }
+        catch(Exception e){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PutMapping("/update-image")
+    public ResponseEntity<ImageDTO> updateImage(@RequestBody ImageDTO imageDTO) {
+        try{
+            imageService.createOrUpdateImage(imageDTO);
+            return new ResponseEntity<>(imageDTO, HttpStatus.CREATED);
+        }
+        catch(Exception e){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @DeleteMapping("/delete-image")
+    public ResponseEntity<ImageDTO> deleteImage(@RequestBody Long id) {
+        try{
+            imageService.deleteImageById(id);
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+        catch(Exception e){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+    }
+
+
 }
--- src/main/java/com/example/serverapi/database/service/ImageService.java
+++ src/main/java/com/example/serverapi/database/service/ImageService.java
@@ -1,24 +1,90 @@
 package com.example.serverapi.database.service;
 
 import com.example.serverapi.database.repository.ImageRepository;
+import com.example.serverapi.database.repository.ListingRepository;
+import com.example.serverapi.dto.ImageDTO;
 import com.example.serverapi.model.Image;
+import com.example.serverapi.model.Listing;
+import com.example.serverapi.utils.converter.DtoAssembler;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class ImageService {
-    @Autowired
-    private ImageRepository imageRepository;
 
-    public void createOrUpdate(Image image) {
-        imageRepository.save(image);
+    private final ImageRepository imageRepository;
+    private final DtoAssembler dtoAssembler;
+    private final ListingRepository listingRepository;
+
+    @Autowired
+    public ImageService(ImageRepository imageRepository, DtoAssembler dtoAssembler, ListingRepository listingRepository) {
+        this.imageRepository = imageRepository;
+        this.dtoAssembler = dtoAssembler;
+        this.listingRepository = listingRepository;
     }
+
+    @Transactional
+    public Image createOrUpdateImage(ImageDTO imageDTO) {
+        try{
+
+            Image image = dtoAssembler.getImageEntity(imageDTO);
+            if(imageDTO.getListingId() != null ) {
+                Optional<Listing> existence = listingRepository.findById(imageDTO.getListingId());
+                if (existence.isPresent()) {
+                    Listing listing = existence.get();
+                    image.setListing(listing);
+                }
+            }
+
+
+            return imageRepository.save(image);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        return null;
+
+    }
+
+    public Image createOrUpdateImage(Image image) {
+        return imageRepository.save(image);
+    }
+
+    public ImageDTO getImageDTOById(Long id) {
+        try{
+            Optional<Image> existence = imageRepository.findById(id);
+            if(existence.isPresent()){
+                Image image = existence.get();
+                ImageDTO imageDTO = dtoAssembler.getImageDTO(image);
+                return imageDTO;
+            }
+            else{
+                throw new EntityNotFoundException("Image not found");
+            }
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+
+    public Optional<Image>  getImageById(Long id) {
+        return imageRepository.findById(id);
+    }
+
 
     public Image getImageByImageUrl(String imageUrl) {
         return imageRepository.findByImageUrl(imageUrl);
     }
 
-    public void deleteImage(Image image) {
-        imageRepository.delete(image);
+    public void deleteImageById(Long id) {
+        imageRepository.deleteById(id);
     }
+
+
 }
