d815dcb50cd5680b1fbb48045b13d34b53826481
Agustin <aggarcia997@gmail.com>
2024-06-18 07:59:22 -0300

    Se implemento una tabla extra para product, que indica la dificultad de un juego

---
+++ src/main/java/com/example/serverapi/model/Difficulty.java
@@ -0,0 +1,26 @@
+package com.example.serverapi.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Difficulty implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String difficultyName;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE}, mappedBy = "difficulty")
+    private List<Product> productList;
+}
