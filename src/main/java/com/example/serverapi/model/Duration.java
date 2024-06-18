180364d1f0809863101cb76d481622b08e61df8b
Agustin <aggarcia997@gmail.com>
2024-06-18 07:58:41 -0300

    Se implemento una tabla extra para product, que indica la duracion de un juego

---
+++ src/main/java/com/example/serverapi/model/Duration.java
@@ -0,0 +1,26 @@
+package com.example.serverapi.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="duration")
+public class Duration {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String durationName;
+
+    @OneToMany(cascade = {CascadeType.MERGE, CascadeType.PERSIST}, fetch = FetchType.LAZY, mappedBy = "duration")
+    private List<Product> productList;
+}
